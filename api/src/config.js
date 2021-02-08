fed1153a2d83ad3aad2bed2e2c8dc97985753964
vitor-risso <vitor.risso13@gmail.com>
2021-02-08 19:18:27 -0300

    .

--- api/src/config.js
+++ api/src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports={
   connectionString: 'mongodb://localhost:27017/node-store',
-  sendgridKey: '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',
-  containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=cs21003200114e82215;AccountKey=GleZ36ge7xejb+rbeoACQaJ0N2YdJNrrAb1cSezIChHY8rNXBIHNTr/+2MiQxo+ZaA1mzX3PMb6sQZ033zmRYg==;EndpointSuffix=core.windows.net'
+  sendgridKey: process.env.SENDGRIDKEY,
+  containerConnectionString: process.env.CONTAINERCONNECTIONSTRING
 }
